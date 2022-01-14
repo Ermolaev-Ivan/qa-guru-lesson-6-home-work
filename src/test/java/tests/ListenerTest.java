572473eeabf60221fd0f1dbccac6291ad92ffcb8
Ivan Ermolaev <ermolaev_i@garpix.com>
2022-01-14 10:06:19 +0300

    Listener for Selenid

---
+++ src/test/java/tests/ListenerTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class ListenerTest {
+
+    @Test
+    @Owner("Ermolaev Ivan")
+    @Story("Проверка наличия вкладки Issues в репозитории")
+    @DisplayName("Проверка наличия вкладки Issues в репозитории листенером")
+    public void IssueNameValidationSelenideTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).shouldBe(Condition.visible);
+        $(partialLinkText("Issues")).shouldHave(text("Issues"));
+    }
+
+}
