265f262d82a011f9e70fe90c8f92b0cef6271df3
Ivan Ermolaev <ermolaev_i@garpix.com>
2022-01-14 09:09:53 +0300

    selenide test

---
+++ src/test/java/tests/CleanSelenideTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class CleanSelenideTest {
+
+    @Test
+    public void IssueNameValidationSelenideTest() {
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).shouldBe(Condition.visible);
+        $(partialLinkText("Issues")).shouldHave(text("Issues"));
+    }
+
+}
